4f84fa1ff1fdbf2c67a6027750b56b9b6e7a667c
zafe312 <borboruahzafe312@gmail.com>
2025-04-19 22:40:01 +0530

    Prompt formatter function created.

--- backend/main.py
+++ backend/main.py
@@ -13,15 +13,26 @@ client = OpenAI(
     api_key=OPENAI_API_KEY,
 )
 
-completion = client.chat.completions.create(
-    model="deepseek-ai/DeepSeek-V3-0324",
-    messages=[
-        {
-            "role": "user",
-            "content": "What is the capital of France?"
-        }
-    ],
-    max_tokens=512,
-)
+def chat_completion(content):
+    completion = client.chat.completions.create(
+        model="deepseek-ai/DeepSeek-V3-0324",
+        messages=[
+            {
+                "role": "user",
+                "content": content
+            }
+        ],
+        max_tokens=512,
+    )
+    return completion.choices[0].message.content
 
-print(completion.choices[0].message)
+def promt_generator(format,tone,topic):
+    return f"Write a {format} in a {tone} tone about {topic}. Do not answer anything else just the {format}."
+
+if __name__=="__main__":
+    format = "caption"
+    tone = "enthusiastic"
+    topic = "my picture"
+    content = promt_generator(format,tone,topic)
+    message = chat_completion(content)
+    print(message)
